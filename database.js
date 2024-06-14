25cd450a1d7c98682d395485c82f5388d3e112cc
JanmejayR <121809920+JanmejayR@users.noreply.github.com>
2024-06-14 14:17:59 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',    // replace with your PostgreSQL username
+    user: 'postgres',    
     host: 'localhost',
-    database: 'bankdb',      // replace with your PostgreSQL database name
-    password: '0101',// replace with your PostgreSQL password
+    database: 'bankdb',      
+    password: '0101',
     port: 5432,
   });
   module.exports = pool;
